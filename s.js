d56259eb58041c3f0d6ac3ea4d7fdb141a65e5eb
Amalxy <amalnathxyxxplay2gmail.com>
2022-10-26 22:43:20 +0530

    working

--- s.js
+++ s.js
@@ -1,10 +1,25 @@
 /////////////////////////////////////////////////////////////////////
 async function x1() {
-  const canvas = await document.getElementById("canvs");
+  var canvas = await document.getElementById("canvs");
   const context = await canvas.getContext("2d");
   const video = document.getElementById("videoElement");
-    const model = await handpose.load();
-    /////////////////////////////////////////////////////////////////
+  const vid1 = document.getElementById("vid1");
+  const imghop= document.getElementById("img1");
+  const imghcl= document.getElementById("img2");
+  const forw= document.getElementById("img3");
+  const back= document.getElementById("img4");
+  const sound= document.getElementById("img5");
+  const add= document.getElementById("img6");
+  const minus= document.getElementById("img7");
+  let vp=1;
+  
+  
+  const model = await handpose.load();
+   let delay=100;
+   var hx=0;
+   var hy=0;
+    
+    context.fill();
     if (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia) {
               navigator.mediaDevices.getUserMedia({ video:{width:350,height:250,},
                     audio:false })
@@ -17,48 +32,64 @@ async function x1() {
                   console.log("Something went wrong!");
                 });
             }
-           
-   // canvas.height=body.height;
-   // canvas.width=body.width;
-    ///////////////////////////////////////////////////////////////
+            let vidsc=['v2.mp4','a.mp4',"vid3.mp4","vid4.mp4"];
+                
+            
     async function draw(){
-        //context.drawImage(video,0,0,250,350);
+  
         context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(back,canvas.width/15,canvas.height/2,10,15);
+        context.drawImage(forw,9*canvas.width/10,canvas.height/2,10,15);
+        context.drawImage(sound,(9*canvas.width/10)-canvas.width/10,9*canvas.height/10,10,15);
+        context.drawImage(add,(9*canvas.width/10),9*canvas.height/10,10,15);
+        context.drawImage(minus,(9*canvas.width/10)-canvas.width/5,9*canvas.height/10,10,15);
         const predictions = await model.estimateHands(video,{
           flipHorizontal: false,
          
          });
-        console.log(predictions);
-        
-        //////////////////////////////////////////////////////s/////
+        //console.log(predictions);
+      
         if (predictions.length > 0) {
-          if(hold(predictions)==true){
           
-          predictions.forEach((prediction) => {
-       // drawHand(predictions,context);
-       for (let i = 0; i < prediction.landmarks.length; i++) {
-        // Get x point
-        const x = canvas.width-prediction.landmarks[i][0];
-        // Get y point
-        const y =prediction.landmarks[i][1];
-        // Start drawing
-        context.beginPath();
-        context.arc(x, y, style[i]["size"], 0, 3 * Math.PI);
-        // Set line color
-        context.fillStyle = style[i]["color"];
-        context.fill();
-                 }
-             });
-            }
+          
+          predictions.forEach((prediction)=>{
 
-        }
-        }
-            //////////////////////////////////////////////////////
-        
-        setInterval(draw,20);
-        /////////////////////////////////////////////////////////
+            
+           
+      
     
-}
+        hx = 30+canvas.width-(prediction.landmarks[0][0]+prediction.landmarks[17][0])/2;
+      
+        hy =-50+(prediction.landmarks[0][1]+prediction.landmarks[9][1])/2;
+        context.drawImage(imghop,hx,hy,15,10);
+       
+        
+        if(hold(prediction)==true){
+        context.drawImage(imghcl,hx,hy,15,10);
+
+          if(addc(hx,hy,canvas)==true){console.log("plus sound"); if(vid1.volume<1) vid1.volume=vid1.volume+.2;  delay();}
+          if(minusc(hx,hy,canvas)==true){console.log("minus");  if(vid1.volume>.3){ vid1.volume=vid1.volume-.2; console.log(vid1.volume); delay();}}
+          if(forwc(hx,hy,canvas)==true){console.log("forw"); vp=(vp+1) % vidsc.length; vid1.src=vidsc[vp]; vid1.play(); delay();}
+          if(backc(hx,hy,canvas)==true){console.log("back");vp=(vp-1) % vidsc.length; vp=Math.sqrt(vp*vp); vid1.src=vidsc[vp]; vid1.play(); delay();}
+          if(soundc(hx,hy,canvas)==true){console.log("sound"); if(vid1.muted==false){vid1.muted=true;}
+          else{vid1.muted=false;}delay();}
+          if(playp(hx,hy,canvas)==true){console.log("play");
+               if(vid1.paused ==true){vid1.play(); } else{vid1.pause();}
+               delay();
+          }}
+             });
+                   }
+                   function delay(){
+                    let i=0;
+                    for(i=0;i<10000000;i++){
+          
+                    }
+                  }
+
+                     }
+                       setInterval(draw,delay);
+        
+                                                  }
 x1();
 
 const fingerJoints = {
@@ -69,60 +100,14 @@ const fingerJoints = {
     pinky: [0, 17, 18, 19, 20],
   };
   
-  // Infinity Gauntlet Style
-  const style = {
-    0: { color: "yellow", size: 10 },1: { color: "gold", size: 6 },2: { color: "green", size: 10 },3: { color: "gold", size: 6 },4: { color: "gold", size: 6 },
-    5: { color: "purple", size: 10 },6: { color: "gold", size: 6 },7: { color: "gold", size: 6 },8: { color: "yellow", size: 6 },9: { color: "blue", size: 10 },
-    10: { color: "gold", size: 6 },11: { color: "gold", size: 6 },12: { color: "gold", size: 6 },13: { color: "red", size: 10 },14: { color: "gold", size: 6 },
-    15: { color: "gold", size: 6 },16: { color: "gold", size: 6 },17: { color: "orange", size: 10 },18: { color: "gold", size: 6 },
-    19: { color: "gold", size: 6 },20: { color: "gold", size: 6 },
-  };
   
-const drawHand = (predictions, ctx) => {
-    // Check if we have predictions
-    if (predictions.length > 0) {
-      
-      // Loop through each prediction
-      predictions.forEach((prediction) => {
-        // Grab landmarks
-        const landmarks = prediction.landmarks;
-  
-        // Loop through fingers
-        for (let j = 0; j < Object.keys(fingerJoints).length; j++) {
-          let finger = Object.keys(fingerJoints)[j];
-          //  Loop through pairs of joints
-          for (let k = 0; k < fingerJoints[finger].length - 1; k++) {
-            // Get pairs of joints
-            const firstJointIndex = fingerJoints[finger][k];
-            const secondJointIndex = fingerJoints[finger][k + 1];
-  
-            // Draw path
-            ctx.beginPath();
-            ctx.moveTo(
-              landmarks[firstJointIndex][0],
-              landmarks[firstJointIndex][1]
-            );
-            ctx.lineTo(
-              landmarks[secondJointIndex][0],
-              landmarks[secondJointIndex][1]
-            );
-            ctx.strokeStyle = "plum";
-            ctx.lineWidth = 4;
-            ctx.stroke();
-          }
-        }
-  
-        // Loop through landmarks and draw em
 
-      });
-    }
-  };
   
-  const hold =(predictions)=>{
-    var fingpx=0;
-    var fingpy=0;
-    var ceny=0;
-    predictions.forEach((prediction) => {
+  const hold =(prediction)=>{
+    let fingpx=0;
+   let fingpy=0;
+   let ceny=0;
+   
    
       for (let i = 5; i <= 13; i=i+4) {
     fingpx +=prediction.landmarks[i+3][0] - prediction.landmarks[i][0];
@@ -131,11 +116,61 @@ const drawHand = (predictions, ctx) => {
   
   }
       
-});
+
   if(fingpy < ceny)
 
-  {return true;}
+  {return  true;}
   else{return false;}
 
 
 };
+
+const playp=(x,y,canvas)=>{
+   y=  y - canvas.height/2;
+   x=  x - canvas.width/2;
+  if(( -10 < x) && (x < 10) && (-10 < y) && (y < 10))
+  {return  true;}
+  else {return false;}
+};
+
+const forwc=(x,y,canvas)=>{
+x=x-9*canvas.width/10;
+y=y-canvas.height/2;
+if(( -10 < x) && (x < 10) && (-10 < y) && (y < 10))
+{return  true;}
+else {return false;}
+};
+
+const backc=(x,y,canvas)=>{
+  x=x-canvas.width/15;
+  y=y-canvas.height/2;
+  if(( -10 < x) && (x < 10) && (-10 < y) && (y < 10))
+  {return  true;}
+  else {return false;}
+  };
+
+  const soundc=(x,y,canvas)=>{
+    x=x-(9*canvas.width/10-canvas.width/10);
+    y=y-9*canvas.height/10;
+    if(( -10 < x) && (x < 10) && (-10 < y) && (y < 10))
+    {return  true;}
+    else {return false;}
+    };
+
+    const addc=(x,y,canvas)=>{
+      x=x-(9*canvas.width/10);
+      y=y-9*canvas.height/10;
+      if(( -10 < x) && (x < 10) && (-10 < y) && (y < 10))
+      {return  true;}
+      else {return false;}
+      };
+
+      const minusc=(x,y,canvas)=>{
+        x=x-(7*canvas.width/10);
+        y=y-9*canvas.height/10;
+        if(( -10< x) && (x < 10) && (-10 < y) && (y < 10))
+        {return  true;}
+        else {return false;}
+        };
+
+       
